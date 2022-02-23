418b7e48063187274e00f19536ac9f08dd605427
felixrydberg <felixryd@gmail.com>
2022-02-23 14:32:21 +0100

    Add Prod class with construc. Started work on app

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 import Cart from './modules/Cart.js';
+import Products from './modules/Products.js';
+
+(function () {
+  (function () {
+    let products = [];
+    fetch(
+      'https://shoppingsite-457c2-default-rtdb.europe-west1.firebasedatabase.app/.json'
+    )
+      .then((r) => r.json())
+      .then((d) => {
+        for (let key in d) {
+          // console.log(d[key]);
+          const { amount, id, name, price, url } = d[key];
+          const product = new Products(amount, id, name, price, url);
+          products.push(product);
+        }
+      })
+      .catch();
+    products.map((e) => console.log(e.id));
+    console.log('this work?');
+  })();
+})();
---
+++ modules/Products.js
@@ -0,0 +1,40 @@
+export default class Products {
+  #amount;
+  #id;
+  #name;
+  #price;
+  #url;
+  constructor(amount, id, name, price, url) {
+    this.#amount = amount;
+    this.#id = id;
+    this.#name = name;
+    this.#price = price;
+    this.#url = url;
+  }
+  getAmount() {
+    return this.#amount;
+  }
+  getId() {
+    return this.#id;
+  }
+  getName() {
+    return this.#name;
+  }
+  getPrice() {
+    return this.#url;
+  }
+  getUrl() {
+    return this.#price;
+  }
+  setAmount(amount) {
+    this.#amount = amount;
+  }
+}
+
+// TODO:
+// Egenskap:
+// Privata variabler, amount name etc
+
+// Metod:
+// Setter 1
+// 5x Getter Amount, name, price, url, id, Products

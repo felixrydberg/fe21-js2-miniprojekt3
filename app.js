ab0f828c0d633cf65b2e7c77d0d381f7a88dba50
felixrydberg <felixryd@gmail.com>
2022-02-23 15:51:25 +0100

    Add html structure and styling, prod async func

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ import Cart from './modules/Cart.js';
 import Products from './modules/Products.js';
 
 (function () {
-  (function () {
+  (async function () {
     let products = [];
-    fetch(
+    await fetch(
       'https://shoppingsite-457c2-default-rtdb.europe-west1.firebasedatabase.app/.json'
     )
       .then((r) => r.json())
       .then((d) => {
         for (let key in d) {
-          // console.log(d[key]);
           const { amount, id, name, price, url } = d[key];
-          const product = new Products(amount, id, name, price, url);
-          products.push(product);
+          products.push(new Products(amount, id, name, price, url));
         }
+        spawnProducts(products);
       })
-      .catch();
-    products.map((e) => console.log(e.id));
-    console.log('this work?');
+      .catch((error) => alert(`gaming: ${error}`));
   })();
+
+  function spawnProducts(data) {}
 })();

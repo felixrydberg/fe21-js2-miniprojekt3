181ffac80d8f0314b99a7d38d6dd30fd2ddb6bb6
Jimmy Wallener <38855083+JimmyWallener@users.noreply.github.com>
2022-02-26 11:47:09 +0100

    [Refactor] separate db access to own file

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 // @ts-nocheck
 import Cart from './modules/Cart.js';
 import Products from './modules/Products.js';
+import { firebase } from './modules/DB.js';
 
 (function () {
   // Firebase object which enables updating database
-  const firebase = {
-    url: 'https://shoppingsite-457c2-default-rtdb.europe-west1.firebasedatabase.app/.json',
 
-    getData: async function () {
-      return await fetch(this.url);
-    },
-    patchData: async function (data) {
-      return await fetch(this.url, {
-        method: 'PATCH',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      });
-    },
-  };
   // On load, get data from database and create array of products
   (async () => {
     let products = [];
